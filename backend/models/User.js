6f3bc3c3194c7d24daa925427c78260fc152574a
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-11-12 16:53:10 +0100

    Add User Schema for mongoose

---
+++ backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  socials: Object,
+  profile_picture: String,
+  bio: { type: String, default: "" },
+  dateOfBirth: { type: Date, required: true },
+  registeredAt: { type: Date, default: new Date() },
+  roles: Array,
+});
+
+module.exports = mongoose.model("User", UserSchema);

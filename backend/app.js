eac0f66e7d09b1e1bc4a61eab47f9531edcc845d
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-11-12 15:37:48 +0100

    Set app.js up

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,65 @@
+const http = require("http");
 const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
 const app = express();
-const port = 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+const port = 5000;
+app.set("port", port);
+
+const indexRouter = require("./routes/index.js");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
 });
 
-app.listen(port, () => {
-  console.log("running on port 5001");
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  return res.json({
+    success: false,
+    error: err.status || 500,
+  });
 });
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
+
+module.exports = app;

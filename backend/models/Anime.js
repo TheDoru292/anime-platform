392fe647978c40432c7c68c0c34c2a8941fd2367
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2023-12-03 12:25:45 +0100

    remove anime source schema

--- backend/models/Anime.js
+++ backend/models/Anime.js
@@ -5,7 +5,6 @@ const AnimeSchema = new Schema({
   title: { type: String, required: true },
   cover: { type: String },
   type: { type: Schema.Types.ObjectId, ref: "AnimeType" },
-  source: { type: Schema.Types.ObjectId, ref: "AnimeSource" },
   genres: [{ type: Schema.Types.ObjectId, ref: "AnimeGenre" }],
   themes: [{ type: Schema.Types.ObjectId, ref: "AnimeTheme" }],
   status: { type: String, required: true },

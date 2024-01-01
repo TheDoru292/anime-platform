0eab55c89d26d2a5e6d6574cbc45b5f6dbcf2c58
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2024-01-01 13:47:27 +0100

    Checks for anime type, genres and themes

---
+++ backend/lib/Checks.js
@@ -0,0 +1,68 @@
+const Type = require("../models/AnimeType");
+const Genre = require("../models/AnimeGenre");
+
+exports.checkAnimeType = (req, res, next) => {
+  if (req.body.type) {
+    Type.findOne({ _id: req.body.type })
+      .then((type) => {
+        if (!type) {
+          return res.status(400).json({
+            success: false,
+            error: "Provided type does not exist!",
+          });
+        }
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          success: false,
+          error: "Please check the type you have sent.",
+        });
+      });
+  }
+
+  next();
+};
+
+exports.checkAnimeGenres = (req, res, next) => {
+  if (req.body.genres) {
+    for (let i = 0; req.body.genres.length > i; i++) {
+      Genre.find({ _id: req.body.genres[i] })
+        .then((genre) => {
+          if (!genre) {
+            return res.status(400).json({
+              success: false,
+              error: "Provided genre(s) do not exist!",
+            });
+          }
+        })
+        .catch((err) => {
+          return res.status(400).json({
+            success: false,
+            error: "Please check the genre type you have sent.",
+          });
+        });
+    }
+  }
+};
+
+exports.checkAnimeThemes = (req, res, next) => {
+  if (req.body.themes) {
+    for (let i = 0; req.body.themes.length > i; i++) {
+      Theme.find({ _id: req.body.themes[i] })
+        .then((theme) => {
+          if (!theme) {
+            return res.status(400).json({
+              success: false,
+              error: "Provided theme(s) do not exist!",
+            });
+          }
+        })
+        .catch((err) => {
+          return res.status(400).json({
+            success: false,
+            error: "Please check the theme type you have sent.",
+          });
+        });
+    }
+  }
+};

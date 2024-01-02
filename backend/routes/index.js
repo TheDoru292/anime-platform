75246d17f3dde61ac8b19f1bedf8e2f41641ae40
TheDoru <96445851+TheDoru292@users.noreply.github.com>
2024-01-02 13:52:51 +0100

    Added first ver of manga type router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,6 +13,7 @@ const animeTypeRouter = require("./animetype");
 const animeGenreRouter = require("./animegenre");
 const animeThemeRouter = require("./animetheme");
 const animeRouter = require("./anime");
+const mangaTypeRouter = require("./mangatype");
 const mangaGenreRouter = require("./mangagenre");
 
 router.use("/auth", authRouter);
@@ -24,6 +25,8 @@ router.use("/anime-type", animeTypeRouter);
 router.use("/anime-genre", animeGenreRouter);
 
 router.use("/anime-theme", animeThemeRouter);
+
+router.use("/manga-type", mangaTypeRouter);
 
 router.use("/manga-genre", mangaGenreRouter);
 
---
+++ backend/routes/mangatype.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
+
+const Type = require("../models/MangaType");
+
+router.post("/new", [
+  body("name").isLength({ min: 1 }).escape(),
+
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.json({ succes: false, errors: errors.array() });
+    }
+
+    Type.create({ name: req.body.name })
+      .then((type) => {
+        return res.status(200).json({ success: true, type });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+]);
+
+router.delete("/:typeName", (req, res) => {
+  Type.deleteOne({ name: req.params.typeName })
+    .then(() => {
+      return res.status(200).json({ success: true, message: "Type deleted." });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
